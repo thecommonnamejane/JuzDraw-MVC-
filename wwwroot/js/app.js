185ab0e339dc7c3a7af65830a437b734a025c56f
thecommonnamejane <thecommonnamejane@gmail.com>
2020-11-01 02:31:26 +0800

    Add project files.

---
+++ wwwroot/js/app.js
@@ -0,0 +1,13 @@
+/*const app = {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    mounted() {
+        console.log("vue");
+    }
+}
+
+Vue.createApp(app).mount('#app')*/ 
+//# sourceMappingURL=app.js.map
---
+++ wwwroot/js/app.ts
@@ -0,0 +1,13 @@
+﻿
+/*const app = {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    mounted() {
+        console.log("vue");
+    }
+}
+
+Vue.createApp(app).mount('#app')*/
